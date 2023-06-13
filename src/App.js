ef1e13505e583f67341524d87e348eae3cc4a57e
JARAYTIDA <rajadityakumawat@gmail.com>
2023-06-13 20:53:30 +0530

    navbar and footer done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 
-import {Navbar} from './components'
-import {Home, Equity, Commodity, Debt} from './pages'
+import {Navbar, Footer} from './components'
+import {Home, Equity, Commodity, Debt, ContactUs, AboutUs, PrivacyPolicy} from './pages'
 
 const App = () => {
     return (
@@ -13,7 +13,11 @@ const App = () => {
                 <Route path = "/commodity" element = {<Commodity/>} />
                 <Route path = "/equity" element = {<Equity/>} />
                 <Route path = "/debt" element = {<Debt/>} />
+                <Route path = "/about-us" element = {<AboutUs/>} />
+                <Route path = "/contact-us" element = {<ContactUs/>} />
+                <Route path = "/privacy-policy" element = {<PrivacyPolicy/>} />
             </Routes>
+            <Footer/>
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {FaTwitter} from 'react-icons/fa'
+import {AiFillInstagram, AiFillLinkedin} from 'react-icons/ai'
 
 
 const Navbar = () => {
     return (
         <div>
-            <div>
-                Parasmanee
-            </div>
+            <div className='flex bg-black text-white text-2xl justify-between'>
 
-            <div>
+                <div className='flex mx-6 my-6 font-bold underline text-4xl place-items-center'>
+                    <Link to="/">
+                        <h1>PRS</h1>
+                    </Link>
+                </div>
+                
+                <div className='flex mx-6 my-6 place-items-center'>
+                    <Link to="/commodity" className='mx-6'>Commodity</Link>
+                    <Link to="/debt" className='mx-6'>Debt</Link>
+                    <Link to="/equity" className='mx-6'>Equity</Link>
+                </div>
+
+                <div className='flex mx-6 my-6 place-items-center'>
+                    <Link to = "https://www.twitter.com/"> <FaTwitter className='mx-3'/> </Link>
+                    <Link to = "https://www.instagram.com/"> <AiFillInstagram className='mx-3'/> </Link>
+                    <Link to = "https://www.linkedin.com/"> <AiFillLinkedin className='mx-3' /> </Link>
+                </div>
 
             </div>
         </div>
